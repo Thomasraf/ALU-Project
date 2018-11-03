ea53a1bb46b186f619a389ddebc0d052734ef30d
Thomasraf <37896696+Thomasraf@users.noreply.github.com>
2018-11-03 17:09:52 +0800

    Add files via upload

--- Input.java
+++ Input.java
@@ -29,39 +29,77 @@ public class Input {
 				if(this.getfloatingPoint().contains("x") == true && half1.matches("[01]+") && !half1.startsWith("0")
 						&& half1.length() == 16) { //to check if a base was inputed and if the input was decimal, and NOT binary
 					if(this.getfloatingPoint().charAt(0) != '-') { //positive sign bit 
-						char stringMSD = half1.charAt(0); // 7
-						int MSD = Integer.parseInt(String.valueOf(stringMSD));
+						if(half1.contains("\\.") == true) {
+							int total = half1.length(); //This gets the total amount of how long the string is so 7123456.8976 total is 12(including '.')
+							int index = half1.indexOf('.'); //This gets the index of the '.' which is 7 in this case
+							int adder = (total - 1) - index; //So it will be 11 - 7	
+							
+							String true_half1 = movingDecimal(half1, index); //Simply moving the decimal
+							
+							if(true_half1.length() != 15) { //This is to place the 0's in front of the number if it is not already 16
+								true_half1 = placingZero(true_half1);
+							}
+							char stringMSD = true_half1.charAt(0); // 7
+							int MSD = Integer.parseInt(String.valueOf(stringMSD));
+							
+							eSpecial(base2, adder); //This is simply E' = E + 398 but placed in a function to make it cleaner, but with the addition of a moved decimal point 
+							String finalMSD = Integer.toBinaryString(MSD);
+							
+								if(MSD < 8) {
+									String x0 = Character.toString(base2.charAt(0)); String x1 = Character.toString(base2.charAt(1)); String x2 = Character.toString(finalMSD.charAt(1)); 
+									String x3 = Character.toString(finalMSD.charAt(2));	String x4 = Character.toString(finalMSD.charAt(3));
+									String combinationField = x0 + x1 + x2 + x3 + x4; //To add all the bits but I'm sorry if it's brute force
+								}
+						}
 						
-						if(MSD < 8) { //If MSD is less than 8 or 9 rule
+						else {
+							int total = half1.length(); //This gets the total amount of how long the string is so 7123456.8976 total is 12(including '.')
+							int index = half1.indexOf('.'); //This gets the index of the '.' which is 7 in this case
+							int adder = (total - 1) - index; //So it will be 11 - 7	
 							
-							if(half1.contains("\\.") == true) { //this is if the input has a "." which we have to move to increase the exponent count (712345655577.7666)
-								int total = half1.length(); //This gets the total amount of how long the string is so 7123456.8976 total is 12(including '.')
-								int index = half1.indexOf('.'); //This gets the index of the '.' which is 7 in this case
-								int adder = (total - 1) - index; //So it will be 11 - 7		
-								
-								String true_half1 = movingDecimal(half1, index); //This is to move the decimal point to the end 
-								true_half1.replace("\\.", ""); //This is to remove the decimal point at the end
-								
-								if(true_half1.length() != 15) { //This is to place the 0's in front of the number if it is not already 16 ~~~NELLO THERE IS WHERE I STOPPED~~~
-									true_half1 = placingZero(true_half1);
-								}
-								
-								eSpecial(base2, adder); //This is simply E' = E + 398 but placed in a function to make it cleaner, but with the addition of a moved decimal point 
-								String finalMSD = Integer.toBinaryString(MSD);
-								String x0 = Character.toString(base2.charAt(0)); String x1 = Character.toString(base2.charAt(1)); String x2 = Character.toString(finalMSD.charAt(1)); 
-								String x3 = Character.toString(finalMSD.charAt(2));	String x4 = Character.toString(finalMSD.charAt(3));
-								String combinationField = x0 + x1 + x2 + x3 + x4; //To add all the bits but I'm sorry if it's brute force
-								
-								
-								}
+							String true_half1 = movingDecimal(half1, index);
 							
-							else { //If this is the input 7123456555777666
-								e(base2); //This is simply E' = E + 398 but placed in a function to make it cleaner
-								String finalMSD = Integer.toBinaryString(MSD);
+							char stringMSD = true_half1.charAt(0); // 7
+							int MSD = Integer.parseInt(String.valueOf(stringMSD));
+							
+							e(base2);
+							String finalMSD = Integer.toBinaryString(MSD);
+							
+							if(MSD < 8) {
 								String x0 = Character.toString(base2.charAt(0)); String x1 = Character.toString(base2.charAt(1)); String x2 = Character.toString(finalMSD.charAt(1)); 
 								String x3 = Character.toString(finalMSD.charAt(2));	String x4 = Character.toString(finalMSD.charAt(3));
 								String combinationField = x0 + x1 + x2 + x3 + x4; //To add all the bits but I'm sorry if it's brute force
 							}
+							
+						}
+//						if(MSD < 8) { //If MSD is less than 8 or 9 rule
+							
+//							if(half1.contains("\\.") == true) { //this is if the input has a "." which we have to move to increase the exponent count (712345655577.7666)
+//								int total = half1.length(); //This gets the total amount of how long the string is so 7123456.8976 total is 12(including '.')
+//								int index = half1.indexOf('.'); //This gets the index of the '.' which is 7 in this case
+//								int adder = (total - 1) - index; 	
+//								
+//								String true_half1 = movingDecimal(half1, index); //This is to move the decimal point to the end 
+//								true_half1.replace("\\.", ""); //This is to remove the decimal point at the end
+//								
+//								
+//								
+//								eSpecial(base2, adder); //This is simply E' = E + 398 but placed in a function to make it cleaner, but with the addition of a moved decimal point 
+//								String finalMSD = Integer.toBinaryString(MSD);
+//								String x0 = Character.toString(base2.charAt(0)); String x1 = Character.toString(base2.charAt(1)); String x2 = Character.toString(finalMSD.charAt(1)); 
+//								String x3 = Character.toString(finalMSD.charAt(2));	String x4 = Character.toString(finalMSD.charAt(3));
+//								String combinationField = x0 + x1 + x2 + x3 + x4; //To add all the bits but I'm sorry if it's brute force
+//								
+//								
+//								}
+//							
+//							else { //If this is the input 7123456555777666
+//								e(base2); //This is simply E' = E + 398 but placed in a function to make it cleaner
+//								String finalMSD = Integer.toBinaryString(MSD);
+//								String x0 = Character.toString(base2.charAt(0)); String x1 = Character.toString(base2.charAt(1)); String x2 = Character.toString(finalMSD.charAt(1)); 
+//								String x3 = Character.toString(finalMSD.charAt(2));	String x4 = Character.toString(finalMSD.charAt(3));
+//								String combinationField = x0 + x1 + x2 + x3 + x4; //To add all the bits but I'm sorry if it's brute force
+//							}
 								
 						
 						} //This is where if(MSD < 8) ends
@@ -70,7 +108,6 @@ public class Input {
 				
 					
 			}
-	}
 
 
 
